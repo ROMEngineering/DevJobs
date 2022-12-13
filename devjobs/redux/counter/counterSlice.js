461affc8f434528433130e14a00edbebf2bde59e
Recep GOCMEN <recep.gocmen@b2metric.com>
2022-12-13 23:59:20 +0300

    redux configs have added

--- devjobs/redux/counter/counterSlice.js
+++ devjobs/redux/counter/counterSlice.js
@@ -14,12 +14,12 @@ export const counterSlice = createSlice({
         decrement: (state) => {
             state.value -= 1;
         },
-        incrementByAmount: (state, action) => {
-            state.value += action.payload;
+        incrementByAmount: (state, { payload }) => {
+            state.value += payload;
         },
     },
 });
 
+export const selectCounter = (state) => state.counter.value;
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
-
 export default counterSlice.reducer;
