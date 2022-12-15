9c5b9152466e041d6573ebd5a483eb3e3b984f8d
ffiruz <ffiruz@seckin.com.tr>
2022-12-15 17:37:52 +0300

    inputs main div created

---
+++ devjobs/commonComponents/SearchInputs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchInputs = () => {
+  return (
+    <div className="main w-full flex justify-center">
+
+    
+    <div className='bg-gray-200 h-24 w-2/3  -mt-10 rounded-md'>
+
+    </div>
+    </div>
+  )
+}
+
+export default SearchInputs
--- devjobs/pages/jobs/index.js
+++ devjobs/pages/jobs/index.js
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchAllJobs } from "../../requestFunctions/fetchAllJobs";
 import Loading from "../../commonComponents/Loading";
 import GeneralHeader from "../../commonComponents/GeneralHeader";
+import SearchInputs from "../../commonComponents/SearchInputs";
 
 const AllJobCards = lazy(() => import("../../commonComponents/AllJobCards"));
 
@@ -17,6 +18,7 @@ const Jobs = () => {
         <Suspense fallback={<Loading />}>
             <>
                 <GeneralHeader />
+                <SearchInputs/>
 
                 <div className="grid grid-cols-3 h-screen justify-center items-center space-4 space-x-4 w-full ">
                     <AllJobCards allData={data} />
