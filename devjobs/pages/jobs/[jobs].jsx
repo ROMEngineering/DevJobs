37b14472973720faef2223dd16d14fa5eb63b53b
Recep GOCMEN <recep.gocmen@b2metric.com>
2022-12-19 22:58:05 +0300

    Big Card component has been created

--- devjobs/pages/jobs/[jobs].jsx
+++ devjobs/pages/jobs/[jobs].jsx
@@ -1,18 +1,37 @@
-import {useFetchCachedQuery} from "../../utilityHooks/useFetchCachedQuery"
-import { useRouter } from 'next/router';
+import React from "react";
+import { useFetchCachedQuery } from "../../utilityHooks/useFetchCachedQuery";
+import { useRouter } from "next/router";
 import { findDeepNestedValue } from "../../utilityFunctions/findDeepNestedValue";
+import GeneralHeader from "../../commonComponents/GeneralHeader";
+import Layout from "../../commonComponents/Layout";
+import ContentHeader from "../../commonComponents/ContentHeader";
 
 const Jobs = () => {
-    const allJobs = useFetchCachedQuery(["jobs"])
+    const allJobs = useFetchCachedQuery(["jobs"]);
     const router = useRouter();
 
     const { jobs: id } = router?.query;
     const selectedJob = findDeepNestedValue(allJobs ?? [], "id", Number(id));
-    
+
+    console.log(selectedJob && selectedJob);
+
     return (
-        <div className="flex flex-col">
-         <p>{JSON.stringify(selectedJob) }</p>
-        </div>
+        <Layout>
+            <GeneralHeader />
+            <section className="flex w-1/2 mx-auto border border-red-500 h-auto">
+                <ContentHeader
+                    data={selectedJob}
+                    // logo={selectedJob.logo}
+                    // company={selectedJob.company}
+                />
+            </section>
+
+            <div className="flex flex-col">
+                <p>{JSON.stringify(selectedJob)}</p>
+            </div>
+
+            <footer className="text-center">Footer</footer>
+        </Layout>
     );
 };
 
--- devjobs/pages/jobs/index.js
+++ devjobs/pages/jobs/index.js
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchAllJobs } from "../../requestFunctions/fetchAllJobs";
 import Loading from "../../commonComponents/Loading";
 import GeneralHeader from "../../commonComponents/GeneralHeader";
+import Layout from "../../commonComponents/Layout";
 
 const AllJobCards = lazy(() => import("../../commonComponents/AllJobCards"));
 
@@ -14,18 +15,17 @@ const Jobs = () => {
     });
 
     return (
-        <Suspense fallback={<Loading />}>
-            <>
-            <GeneralHeader/>
-       
-            <div className="grid grid-cols-3 h-screen justify-center items-center space-4 space-x-4 w-full ">
-               
-                <AllJobCards allData={data} />
-            </div>
+        <Layout>
+            <Suspense fallback={<Loading />}>
+                <>
+                    <GeneralHeader />
 
-            
-            </>
-        </Suspense>
+                    <div className="grid grid-cols-3 h-screen justify-center items-center space-4 space-x-4 w-full ">
+                        <AllJobCards allData={data} />
+                    </div>
+                </>
+            </Suspense>
+        </Layout>
     );
 };
 
